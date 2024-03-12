f8bab42bf02d17646b69710db21d389dd97cd8dd
iino <g-iino@ruby-dev.jp>
2024-03-12 14:15:05 +0900

    引数なし修正および初期値修正

--- main.rb
+++ main.rb
@@ -7,12 +7,18 @@ require 'cgi'
 
 FILE_PATH = 'public/memos.json'
 
-def get_memos(file_path)
-  File.open(file_path) { |f| JSON.parse(f.read) }
+def get_memos
+  if File.exist?(FILE_PATH) && !File.zero?(FILE_PATH)
+    File.open(FILE_PATH) { |f| JSON.parse(f.read) }
+  else
+    sample_file_path = 'public/sample.json'
+    File.exist?(sample_file_path) && !File.zero?(sample_file_path)
+    File.open(sample_file_path) { |f| JSON.parse(f.read) }
+  end
 end
 
-def set_memos(file_path, memos)
-  File.open(file_path, 'w') { |f| JSON.dump(memos, f) }
+def set_memos(memos)
+  File.open(FILE_PATH, 'w') { |f| JSON.dump(memos, f) }
 end
 
 get '/' do
@@ -20,7 +26,7 @@ get '/' do
 end
 
 get '/memos' do
-  @memos = get_memos(FILE_PATH)
+  @memos = get_memos
   erb :index
 end
 
@@ -29,7 +35,7 @@ get '/memos/create' do
 end
 
 get '/memos/:id' do
-  memos = get_memos(FILE_PATH)
+  memos = get_memos
   @title = memos[params[:id]]['title']
   @content = memos[params[:id]]['content']
   erb :detail
@@ -39,16 +45,19 @@ post '/memos' do
   title = params[:title]
   content = params[:content]
 
-  memos = get_memos(FILE_PATH)
-  id = (memos.keys.map(&:to_i).max + 1).to_s
+  memos = get_memos
+  memos ||= {}
+
+  max_key = memos.keys.map(&:to_i).max || 0
+  id = (max_key + 1).to_s
   memos[id] = { 'title' => title, 'content' => content }
-  set_memos(FILE_PATH, memos)
+  set_memos(memos)
 
   redirect '/memos'
 end
 
 get '/memos/:id/edit' do
-  memos = get_memos(FILE_PATH)
+  memos = get_memos
   @title = memos[params[:id]]['title']
   @content = memos[params[:id]]['content']
   erb :edit
@@ -58,17 +67,17 @@ patch '/memos/:id' do
   title = params[:title]
   content = params[:content]
 
-  memos = get_memos(FILE_PATH)
+  memos = get_memos
   memos[params[:id]] = { 'title' => title, 'content' => content }
-  set_memos(FILE_PATH, memos)
+  set_memos(memos)
 
   redirect "/memos/#{params[:id]}"
 end
 
 delete '/memos/:id' do
-  memos = get_memos(FILE_PATH)
+  memos = get_memos
   memos.delete(params[:id])
-  set_memos(FILE_PATH, memos)
+  set_memos(memos)
 
   redirect '/memos'
 end
